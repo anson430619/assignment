126a23e93a2e491ffdb90ebfc5e16df4fb7417d5
anson430619 <144148436+anson430619@users.noreply.github.com>
2023-09-05 21:17:51 +0530

    Day2 assignment submitted

---
+++ 2nd.py
@@ -0,0 +1,16 @@
+#declaring variable a=25 and b=3
+a=25
+b=3
+#performing floor division (//)
+c=a//b
+print("Floor Division of ",a,"//",b,"=",c )
+
+#to find a power b
+d= a ** b
+print(a,"power",b,"is",d)
+
+#to find wether a and b are equal or not
+if (a == b):
+    print("true,they are equal")
+else:
+    print("false,they are not equal")
